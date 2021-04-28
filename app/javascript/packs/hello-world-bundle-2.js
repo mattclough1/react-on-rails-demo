351ffbddd07a24c80640ba6ed4aa6190bd6715c4
Matt Clough <cluffpost@gmail.com>
2021-04-28 14:39:14 -0400

    added register issue example

---
+++ app/javascript/packs/hello-world-bundle-2.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import HelloWorld2 from '../bundles/HelloWorld2/components/HelloWorld2';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  HelloWorld2,
+});
